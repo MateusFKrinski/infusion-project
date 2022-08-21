a0ce13d5fee9c4bca21e11196c3ee1b6f8b7ef51
Mateus Francisco Krinski <mateus.krinski10@gmail.com>
2022-08-20 22:27:55 -0300

    new

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -1,58 +1,63 @@
-function Carousel(config){
+function Carousel(config) {
+  this.container =
+    typeof config.container === "string"
+      ? document.querySelector(config.container)
+      : config.container;
 
-    this.container = ( typeof config.container === 'string') ? document.querySelector(config.container) : config.container
-    
-    this.itens = (typeof config.itens === 'string') ? this.container.querySelectorAll(config.itens) : config.itens
-    
-    this.btnPrev = (typeof config.btnPrev === 'string') ? this.container.querySelector(config.btnPrev) : config.btnPrev
-    
-    this.btnNext = (typeof config.btnNext === 'string') ? this.container.querySelector(config.btnNext) : config.btnNext
-    
-    var _this = this;
-    var _currentSlide = 0
-    
-    init()
-    
-    function init(){
+  this.itens =
+    typeof config.itens === "string"
+      ? this.container.querySelectorAll(config.itens)
+      : config.itens;
 
-        var _show = _this.container.querySelectorAll('.show')
-        
-        Array.prototype.forEach.call(_show, function(sh){
-            sh.classList.remove('show')
-        })
-        _this.itens[0].classList.add('show')
-        _this.btnNext.removeAttribute('style')
-        _this.btnPrev.removeAttribute('style')
-        
-        addListeners() 
-    }
-    
-    function addListeners(){
+  this.btnPrev =
+    typeof config.btnPrev === "string"
+      ? this.container.querySelector(config.btnPrev)
+      : config.btnPrev;
 
-        _this.btnNext.addEventListener('click', showNextSlide)
-        _this.btnPrev.addEventListener('click', showPrevSlide)
-    }
-    
-    function showNextSlide(){
+  this.btnNext =
+    typeof config.btnNext === "string"
+      ? this.container.querySelector(config.btnNext)
+      : config.btnNext;
 
-        _currentSlide++;
-        showSlide()
-    }
-    
-    function showPrevSlide(){
+  var _this = this;
+  var _currentSlide = 0;
 
-        _currentSlide--;
-        showSlide()        
-    }
-    
-    function showSlide(){
+  init();
 
-        var qtd = _this.itens.length;
-        var slide = _currentSlide % qtd;
-        slide = Math.abs(slide);
-        
-        _this.container.querySelector('.show').classList.remove('show');
-        _this.itens[slide].classList.add('show')
-    }
-    
+  function init() {
+    var _show = _this.container.querySelectorAll(".show");
+
+    Array.prototype.forEach.call(_show, function (sh) {
+      sh.classList.remove("show");
+    });
+    _this.itens[0].classList.add("show");
+    _this.btnNext.removeAttribute("style");
+    _this.btnPrev.removeAttribute("style");
+
+    addListeners();
+  }
+
+  function addListeners() {
+    _this.btnNext.addEventListener("click", showNextSlide);
+    _this.btnPrev.addEventListener("click", showPrevSlide);
+  }
+
+  function showNextSlide() {
+    _currentSlide++;
+    showSlide();
+  }
+
+  function showPrevSlide() {
+    _currentSlide--;
+    showSlide();
+  }
+
+  function showSlide() {
+    var qtd = _this.itens.length;
+    var slide = _currentSlide % qtd;
+    slide = Math.abs(slide);
+
+    _this.container.querySelector(".show").classList.remove("show");
+    _this.itens[slide].classList.add("show");
+  }
 }
